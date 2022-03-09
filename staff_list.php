c675c076faef8b04ae41381fd9c414b90e5b957d
hoglet <shuya224@gmail.com>
2022-03-09 14:51:01 +0900

    #1

---
+++ staff_list.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>EC-SITE</title>
+  </head>
+  <body>
+      <?php
+        try{
+            $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+            $user = 'root';
+            $password = '';
+            $dbh = new PDO($dsn, $user, $password);
+            $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // 1は全部という意味
+            $sql='SELECT name FROM mst_staff WHERE 1';
+            //クエリー作成
+            $stmt=$dbh->prepare($sql);
+            //プレースホルダに値をセットし、SQL文を実行
+            $stmt->execute();
+
+            $dbh=null;
+
+            print 'スタッフ一覧 <br/><br/>';
+
+            while(true){
+                // $stmtから1レコードを取り出す
+                $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+                if($rec==false){
+                    break;
+                }
+                print $rec['name'];
+                print '<br/>';
+            }
+
+        }catch(Exception $e){
+            print 'ただいま障害によりご迷惑をお掛けしております。';
+            print $e->getMessage ();
+            exit();
+        }
+      ?>
+  </body>
+</html>
