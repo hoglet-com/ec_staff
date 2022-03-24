0d21ddd7d55f9356d7cf85d3321a96d15fc5f959
hoglet <shuya224@gmail.com>
2022-03-24 22:50:23 +0900

    disp1

--- staff_list.php
+++ staff_list.php
@@ -39,6 +39,8 @@
                 //print '<input type="hidden" name="staffname" value="' . $rec['name'] . '">';
                 print '<br/>';
             }
+
+            print '<input type="submit" name="disp" value="参照">';
             print '<input type="submit" name="add" value="追加">';
             print '<input type="submit" name="edit" value="修正">';
             print '<input type="submit" name="delete" value="削除">';
